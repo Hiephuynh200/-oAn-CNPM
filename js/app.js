5ff00d194eba23e6d5da50b85accf0624e29b4fb
HiepHuynh <huynhhiepvan1998@gmail.com>
2022-11-04 13:28:27 +0700

    second commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
+//$ định nghĩa 1 hàm
+//Basic syntax is: $(selector).action()
+/*
+Examples:
+
+$(this).hide() - hides the current element.
+
+$("p").hide() - hides all <p> elements.
+
+$(".test").hide() - hides all elements with class="test".
+
+$("#test").hide() - hides the element with id="test".
+*/
 $(function () {
     $(window).on('scroll', function () {
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
+            $('.hamburger').addClass('whenscrolled');
             $('nav').addClass('stickyadd');
+            $('nav').removeClass('stick');
         } else {
+            $('.hamburger').removeClass('whenscrolled');
             $('nav').removeClass('stickyadd');
+            $('nav').addClass('stick');
         }
     });
 });
